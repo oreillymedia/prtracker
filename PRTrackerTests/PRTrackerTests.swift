772b62bba0dec321b1d28c2cf8d47d9565ab8d6c
Matt Blackmon <mblackmon@oreilly.com>
2026-04-27 12:38:19 -0500

    Initial Commit

---
+++ PRTrackerTests/PRTrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  PRTrackerTests.swift
+//  PRTrackerTests
+//
+//  Created by Matt Blackmon on 4/27/26.
+//
+
+import Testing
+@testable import PRTracker
+
+struct PRTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
